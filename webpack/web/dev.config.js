36cafa3d38023ba4f4f359be6e2879133a34f4eb
Phong Nguyen <phongn@jobready.com.au>
2019-01-26 20:43:28 +0700

    Update webpack

--- webpack/web/dev.config.js
+++ webpack/web/dev.config.js
@@ -7,9 +7,6 @@ module.exports = function(env) {
         devtool: 'source-map',
         plugins: [
             new Webpack.DefinePlugin({
-                global: {
-                    siteUrl: JSON.stringify('http://localhost:3000/')
-                },
                 process: {
                     env: {
                         NODE_ENV: JSON.stringify('development')
--- webpack/web/prod.config.js
+++ webpack/web/prod.config.js
@@ -2,19 +2,22 @@ const Webpack = require('webpack')
 const WebpackMerge = require('webpack-merge')
 const BaseConfig = require('./base.config.js')
 
-module.exports = function(env) {
+module.exports = function (env) {
     return WebpackMerge(BaseConfig(env.env, env.debug), {
         plugins: [
+            // new BabiliPlugin(),
             new Webpack.DefinePlugin({
-                global: {
-                    siteUrl: JSON.stringify('https://namduongkh.github.io/catch-fruit-game/')
-                },
+                global: {},
                 process: {
                     env: {
                         NODE_ENV: JSON.stringify('production')
                     }
                 }
             }),
+            new Webpack.LoaderOptionsPlugin({
+                minimize: true,
+                debug: false
+            })
         ]
     })
 }
