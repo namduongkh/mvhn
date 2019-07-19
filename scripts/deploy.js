d30092e5be835b0ac7041abbc97119e56cf599a8
Phong Nguyen <phongn@jobready.com.au>
2019-07-19 22:37:46 +0700

    Build App 1563550656703

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,7 +2,7 @@ import Util from "./util";
 
 (function () {
   Util.execCommands([
-    'npm run build:web',
+    // 'npm run build:web',
     'sleep 10',
     `git add . && git commit -am "Build App ${Date.now()}" && git push origin master`,
     `git push heroku master`
