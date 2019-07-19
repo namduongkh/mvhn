aac1938fed3b90c4398fbbb70f45cc27c7c5a550
Phong Nguyen <phongn@jobready.com.au>
2019-07-19 22:44:36 +0700

    Build App 1563551005139

--- scripts/deploy.js
+++ scripts/deploy.js
@@ -2,12 +2,13 @@ import Util from "./util";
 
 (function () {
   Util.execCommands([
-    'npm run clean:prod',
-    'webpack --env.env=prod --env.dir=web --progress --profile --colors',
-    'webpack --env.env=prod --env.dir=cms --progress --profile --colors',
+    'npm run build:web',
+    // 'npm run clean:prod',
+    // 'webpack --env.env=prod --env.dir=web --progress --profile --colors',
+    // 'webpack --env.env=prod --env.dir=cms --progress --profile --colors',
     'sleep 10',
     `git add . && git commit -am "Build App ${Date.now()}"`,
     `git push origin master`,
-    `git push heroku master`
+    // `git push heroku master`
   ])
 })();
