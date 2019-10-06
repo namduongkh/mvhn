0141ee76781d2389d8a112d8509c4f14d11c968d
Phong Nguyen <phongn@jobready.com.au>
2019-10-06 22:36:43 +0700

    MVHN - Update config for template creative

--- app/config/template/creative-cv.js
+++ app/config/template/creative-cv.js
@@ -8,9 +8,8 @@ module.exports = {
     include: {
       css: [
         "https://fonts.googleapis.com/css?family=Montserrat:400,700,200",
-        "public/assets/creative-cv/css/aos.css",
-        "public/assets/creative-cv/css/bootstrap.min.css",
-        "public/assets/creative-cv/styles/main.css",
+        "public/assets/creative-cv/styles/aos.css",
+        "public/assets/creative-cv/styles/bootstrap.min.css",
         'public/vendor/font-awesome/css/font-awesome.min.css',
         'cms/assets/css/all.min.css',
         publicPath + '/styles/vendor.css',
@@ -18,12 +17,11 @@ module.exports = {
       ],
       js: [
         publicPath + '/scripts/vendor.js',
-        "public/assets/creative-cv/js/core/jquery.3.2.1.min.js",
-        "public/assets/creative-cv/js/core/popper.min.js",
-        "public/assets/creative-cv/js/core/bootstrap.min.js",
-        "public/assets/creative-cv/js/now-ui-kit.js?v=1.1.0",
-        "public/assets/creative-cv/js/aos.js",
-        "public/assets/creative-cv/scripts/main.js",
+        "public/assets/creative-cv/scripts/core/jquery.3.2.1.min.js",
+        "public/assets/creative-cv/scripts/core/popper.min.js",
+        "public/assets/creative-cv/scripts/core/bootstrap.min.js",
+        "public/assets/creative-cv/scripts/now-ui-kit.js?v=1.1.0",
+        "public/assets/creative-cv/scripts/aos.js",
         publicPath + '/scripts/main.js',
       ]
     }
--- webpack/web/config/entries.js
+++ webpack/web/config/entries.js
@@ -7,7 +7,7 @@ configManager.setup('./app/config');
 const vendor = configManager.get('web.assets.required');
 
 // let mainResource = Glob.sync(PATHS.module + "/core/+(scripts|css|lib)/+(main.js|styles.scss)");
-let mainResource = Glob.sync(PATHS.template + "/+(*)/+(*.js|*.css|*.scss)");
+let mainResource = Glob.sync(PATHS.template + "/+(css|js)/+(*.js|*.css|*.scss)");
 // mainResource = mainResource.concat(Glob.sync(PATHS.module + "/core/lib/+(*)/+(css|js)/+(*.js|*.css)"));
 mainResource = mainResource.concat(Glob.sync(PATHS.module + "/+(*)/views/+(css|js)/+(*.js|*.scss)"));
 
