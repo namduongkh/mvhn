0916a3ce62d3ff99cb633d2f2a8604a3a3b16311
Phong Nguyen <namduong.kh94@gmail.com>
2020-04-10 10:02:37 +0700

    MVHN - Devtools indexes management

---
+++ app/plugins/devtool/controllers/cms_mongo_indexes_controller.js
@@ -0,0 +1,33 @@
+'use strict';
+
+import mongoose from "mongoose";
+import _ from "lodash";
+import Boom from "boom";
+
+export default class CmsMongoIndexesController extends ResourcesController {
+  async index() {
+    await this.initModel();
+    let indexes = await this.MODEL.collection.getIndexes({ full: true });
+
+    return indexes;
+  }
+
+  async delete() {
+    await this.initModel();
+    let id = this.request.params.id || this.request.query.id;
+
+    await this.MODEL.collection.dropIndex(id);
+
+    return true;
+  }
+
+  async initModel(action = '') {
+    let model = this.request.params.model || this.request.query.model;
+    delete this.request.params.model;
+    delete this.request.query.model;
+
+    if (!model) throw Boom.badRequest("Please provide model param");
+
+    this.MODEL = mongoose.model(model);
+  }
+}
