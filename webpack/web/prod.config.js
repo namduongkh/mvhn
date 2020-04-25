ae90b0f8521a1ca337374c29c474466fad2b0107
Phong Nguyen <namduong.kh94@gmail.com>
2020-04-25 15:57:56 +0700

    MVHN - Change minify tool

--- webpack/web/prod.config.js
+++ webpack/web/prod.config.js
@@ -1,14 +1,11 @@
 const Webpack = require('webpack')
 const WebpackMerge = require('webpack-merge')
 const BaseConfig = require('./base.config.js')
-const ngAnnotatePlugin = require('ng-annotate-webpack-plugin');
-const UglifyJSPlugin = require('uglifyjs-webpack-plugin');
-const BabiliPlugin = require('babili-webpack-plugin');
+const MinifyPlugin = require("babel-minify-webpack-plugin");
 
 module.exports = function(env) {
     return WebpackMerge(BaseConfig(env.env), {
         plugins: [
-            // new BabiliPlugin(),
             new Webpack.DefinePlugin({
                 global: {},
                 process: {
@@ -21,23 +18,7 @@ module.exports = function(env) {
                 minimize: true,
                 debug: false
             }),
-            new UglifyJSPlugin({
-                beautify: false,
-                mangle: false,
-                compress: {
-                    sequences: true,
-                    dead_code: true,
-                    conditionals: true,
-                    booleans: true,
-                    unused: true,
-                    if_return: true,
-                    join_vars: true,
-                    warnings: false,
-                    drop_console: false
-                },
-                comments: false,
-                sourceMap: true
-            })
+            new MinifyPlugin()
         ]
     })
 }
